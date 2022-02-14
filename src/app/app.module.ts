080dc1584dc6cf06f79f0a0182e59a7fbf10f1ca
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-02-14 11:43:17 +0300

    http client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
     HttpClientModule,
   ],
   providers: [],
